df27c1fe7b39eb37a4711275688110391f0933cf
kionou <kionoumamadou.00@gmail.com>
2022-10-25 16:38:41 +0000

    ok

---
+++ src/Components/FormationCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+  <p>bonjour le monde des vivants</p>
+  <p>gkls,g</p>
+  <h1>gsdqgfdhhgng</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
