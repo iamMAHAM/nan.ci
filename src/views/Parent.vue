9fba82902def1e6ecf36d23570fef0122b47a255
Yobson2 <yobouelouiscarlos@gmail.com>
2022-10-27 05:47:52 -0700

    parents

---
+++ src/views/Parent.vue
@@ -0,0 +1,16 @@
+<template>
+   <Parents_idn />
+   
+</template>
+
+<script>
+import Parents_idn from '@/components/Parents_idn.vue'
+export default {
+  components: { Parents_idn },
+
+}
+</script>
+
+<style>
+
+</style>
